626fca71de9b8b29cd5bbf1e4508a05ed921419d
Eduardo Almeida <web2solucoes@gmail.com>
2021-04-12 21:16:53 -0300

    shadow and mut differences

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,6 @@
 fn make_mutable () {
     // transformar em mutavel - make mutable
+    // nao é possivel mudar o tipo de variável
     let mut x = 5;
     println!("The value of x  is {}", x);
 
@@ -10,20 +11,27 @@ fn make_mutable () {
 
 fn shadow () {
     // Sombreamento - shadowing
+    // se tentar atribuir valor a x sem usar let resultará em erro
+    // podemos mudar o tipo de variável
     let x = 5;
 
     let x = x + 1;
 
     let x = x * 2;
 
-    println!("The value of x is {}", x)
+    println!("The value of x is {}", x);
+
+    let x = "abc";
+
+    println!("The value of x is {}", x);
+
+    println!("The length of x is {}", x.len());
 }
 
-fn main() {
+fn main() { 
     
     make_mutable();
 
     shadow();
 
-    
 }
